f9df2b66d144bca8ebec2ab94a7c534dca194da7
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-27 09:50:00 -0700

    Add API class and method to call API based on name.

---
+++ src/api.js
@@ -0,0 +1,21 @@
+class API {
+
+  requestConditionAPI(inputName) {
+    let promise = new Promise(function(resolve, reject) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `https://api.betterdoctor.com/2016-03-01/doctors?name=${inputName}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`);
+      xhr.send();
+      xhr.onload = function() {
+        if (this.status === 200) {
+          let response = JSON.parse(xhr.response);
+          resolve(response);
+        } else {
+          reject(Error(xhr.statusText));
+        }
+      }
+    });
+    return promise;
+  }
+}
+
+export { API };
