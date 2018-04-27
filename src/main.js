738a1c566a911c2de48cb3852b332c2c445040c5
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-27 11:38:01 -0700

    Add logic to return results based on name. Spec 2 met.

--- src/main.js
+++ src/main.js
@@ -6,15 +6,45 @@ import './styles.css';
 import { API } from './api.js';
 
 $(document).ready(function() {
-  $('form').submit(function(event) {
+  $('form.condition button').click(function(event) {
     event.preventDefault();
 
     let inputCondition = $('#condition').val();
-    let nameCall = new API();
+    let conditionCall = new API();
 
-    let promiseCondition = nameCall.requestConditionAPI(inputCondition);
+    let promiseCondition = conditionCall.requestConditionAPI(inputCondition);
 
     promiseCondition.then(function(response) {
+      let responsePracticesArray = response.data;
+      responsePracticesArray.forEach(function(practice) {
+        $('.output').append(
+          `<div class="card">
+            <div class="card-header">
+              <h3>${practice.profile.first_name} ${practice.profile.last_name}, ${practice.profile.title}</h3>
+            </div>
+            <div class="card-body">
+            </div>
+          </div>`
+        );
+        console.log(practice);
+      });
+      }, function(error) {
+      $('div.error').text('There has been an error: ' + error);
+      $('div.error').show();
+    });
+  });
+
+  $('form.name button').click(function(event) {
+    event.preventDefault();
+
+    let inputName = $('#name').val();
+    let nameCall = new API();
+    console.log("inputName is " + inputName);
+
+    let promiseName = nameCall.requestNameAPI(inputName);
+    console.log("promiseName is " + promiseName);
+
+    promiseName.then(function(response) {
       let responsePracticesArray = response.data;
       responsePracticesArray.forEach(function(practice) {
         $('.output').append(
