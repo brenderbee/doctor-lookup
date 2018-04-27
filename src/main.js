05a187d2edc16d17818414abf461a1d7fc32e57c
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-27 15:09:58 -0700

    Specs 8 and 9 met. Add logic to return error if no search results are returned.

--- src/main.js
+++ src/main.js
@@ -16,7 +16,10 @@ $(document).ready(function() {
 
     let promiseCondition = conditionCall.requestConditionAPI(inputCondition);
 
-      promiseCondition.then(function(response) {
+    promiseCondition.then(function(response) {
+      if (response.meta.total === 0) {
+        $('.output').text('Your search returned no results.');
+      } else {
         let responseDoctorsArray = response.data;
         responseDoctorsArray.forEach(function(doctor) {
           let practices = doctor.practices;
@@ -40,11 +43,12 @@ $(document).ready(function() {
             </div>`
           );
         });
-        }, function(error) {
-        $('div.error').text('There has been an error: ' + error);
-        $('div.error').show();
-      });
+      }
+    }, function(error) {
+      $('div.error').text('There has been an error: ' + error);
+      $('div.error').show();
     });
+  });
 
 
   $('form.name button').click(function(event) {
@@ -55,29 +59,34 @@ $(document).ready(function() {
     let promiseName = nameCall.requestNameAPI(inputName);
 
     promiseName.then(function(response) {
-      let responseDoctorsArray = response.data;
-      responseDoctorsArray.forEach(function(doctor) {
-        let practices = doctor.practices;
-        let portlandOffices = parsePortlandOffices(practices);
-        let website = checkWebsite(portlandOffices[0].website);
-        let newPatients = portlandOffices[0].accepts_new_patients;
-        $('.output').append(
-          `<div class="card">
-            <div class="card-header">
-              <h3>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</h3>
-            </div>
-            <div class="card-body">
-              <p><strong>Phone Number:</strong></p>
-              <p>${portlandOffices[0].phones[0].number}</p>
-              <p><strong>Address:</strong></p>
-              <p>${portlandOffices[0].visit_address.street}</p>
-              <p>${portlandOffices[0].visit_address.city}, ${portlandOffices[0].visit_address.state} ${portlandOffices[0].visit_address.zip}</p>
-              <p><strong>Website:</strong> ${website}</p>
-              <p><strong>Accepting new patients:</strong> ${newPatients}</p>
-            </div>
-          </div>`
-        );
-      });
+      if (response.meta.total === 0) {
+        $('.output').text('Your search returned no results.');
+      } else {
+        let responseDoctorsArray = response.data;
+        responseDoctorsArray.forEach(
+          function(doctor) {
+          let practices = doctor.practices;
+          let portlandOffices = parsePortlandOffices(practices);
+          let website = checkWebsite(portlandOffices[0].website);
+          let newPatients = portlandOffices[0].accepts_new_patients;
+          $('.output').append(
+            `<div class="card">
+              <div class="card-header">
+                <h3>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</h3>
+              </div>
+              <div class="card-body">
+                <p><strong>Phone Number:</strong></p>
+                <p>${portlandOffices[0].phones[0].number}</p>
+                <p><strong>Address:</strong></p>
+                <p>${portlandOffices[0].visit_address.street}</p>
+                <p>${portlandOffices[0].visit_address.city}, ${portlandOffices[0].visit_address.state} ${portlandOffices[0].visit_address.zip}</p>
+                <p><strong>Website:</strong> ${website}</p>
+                <p><strong>Accepting new patients:</strong> ${newPatients}</p>
+              </div>
+            </div>`
+          );
+        });
+      }
       }, function(error) {
       $('div.error').text('There has been an error: ' + error);
       $('div.error').show();
