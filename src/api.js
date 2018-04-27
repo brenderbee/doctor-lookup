141e2b8590e55dc47d586faea6821d8620551b0d
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-27 10:24:13 -0700

    Refactor api call for medical condition.

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 class API {
 
-  requestConditionAPI(inputName) {
+  requestAPI(inputCondition) {
     let promise = new Promise(function(resolve, reject) {
       const xhr = new XMLHttpRequest();
-      xhr.open('GET', `https://api.betterdoctor.com/2016-03-01/doctors?name=${inputName}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`);
+      xhr.open('GET', `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputCondition}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`);
       xhr.send();
       xhr.onload = function() {
         if (this.status === 200) {
