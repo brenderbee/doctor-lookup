e0c32447567ad7bfef4db195cf6b6e760933dc08
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-27 11:23:37 -0700

    Add logic for API call based on doctor name.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 class API {
 
-  requestConditionAPI(inputCondition, inputName) {
+  requestConditionAPI(inputCondition) {
     let promise = new Promise(function(resolve, reject) {
       const xhr = new XMLHttpRequest();
       xhr.open('GET', `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputCondition}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`);
@@ -16,6 +16,24 @@ class API {
     });
     return promise;
   }
+
+  requestNameAPI(inputName) {
+    let promise = new Promise(function(resolve, reject) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `https://api.betterdoctor.com/2016-03-01/doctors?name=${inputName}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`);
+      xhr.send();
+      xhr.onload = function() {
+        if (this.status === 200) {
+          let response = JSON.parse(xhr.response);
+          resolve(response);
+        } else {
+          reject(Error(xhr.statusText));
+        }
+      }
+    });
+    return promise;
+  }
+
 }
 
 export { API };
